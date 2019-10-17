4249c12a0858def74ee7a375ea3f5a3ee090c308
Efren Gonzales <colorado3106@gmail.com>
2019-10-17 15:52:14 -0600

    my pwp project

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -6,16 +6,13 @@
 </head>
 	<body>
 		<p>
-			<strong>Purpose:</strong> I am building this PWP (Personal Web Page) to help me obtain gainful employment using my past experience and new knowledge that I am gaining in my
-			deepdive coding class.
-		</p>
+			Purpose: I am building this PWP (Personal Web Page) to help me obtain gainful employment using my past experience and new knowledge that I am gaining in my
+			deepdive coding class.</p>
 		<p>
-			<strong>Audience:</strong> This PWP is to be viewed by potential employers, clients, friends, peers and anyone who wants to know and to understand all experience that I have
-			gained and to highlight all my accomplishments, achievements, and certifications that I have done over the course of my work history.
-		</p>
+			Audience: This PWP is to be viewed by potential employers, clients, friends, peers and anyone who wants to know and to understand all experience that I have
+			gained and to highlight all my accomplishments, achievements, and certifications that I have done over the course of my work history.</p>
 		<p>
-			Goal(s):
+			Goals:
 		</p>
-
 	</body>
 </html>
