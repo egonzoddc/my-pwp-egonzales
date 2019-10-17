b2f7b3c9629e1789965a5d6a97c4087dff92b89d
Efren Gonzales <colorado3106@gmail.com>
2019-10-17 13:19:18 -0600

    my pwp project

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -4,14 +4,15 @@
 	<meta charset="UTF-8">
 	<title>My Personal Web Page</title>
 </head>
-<body>
-<p>
+	<body>
+		<p>
 	Purpose: I am building this PWP (Personal Web Page) to help me obtain gainful employment using my past experience and new knowledge that I am gaining in my
 	deepdive coding class.
-</p>
-<p>
+		</p>
+		<p>
 	Audience: This PWP is to be viewed by potential employers, clients, friends, peers and anyone who wants to know and to understand all experience that I have
 	gained and to highlight all my accomplishments, achievements, and certifications that I have done over the course of my life.
-</p>
-</body>
+		</p>
+
+	</body>
 </html>
