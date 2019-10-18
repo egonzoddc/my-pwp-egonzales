9dc218b5481230a92bf3d270918bdc82d58e2aef
Efren Gonzales <colorado3106@gmail.com>
2019-10-17 21:11:55 -0600

    my pwp project Milestone 1

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -13,13 +13,27 @@
 			<p><strong>Audience:</strong> This PWP is to be viewed by potential employers, clients, friends, peers and anyone who wants to know and to understand all experience that I have
 			gained and to highlight all my accomplishments, achievements, and certifications that I have done over the course of my work history.</p>
 		</div>
-	<h3 align="center"><strong></strong><font size="5">Goals:</font></h3>
-
+		<br>
+	<h1 align="center"><strong></strong><font size="5">Goals:</font></h1>
+<div>
 		<ul align="center">To acquire either full-time, part-time or contract work with either my low voltage or coding skills.</ul>
 		<ul align="center">To showcase work that I have done and completed in both coding and low voltage.</ul>
 		<ul align="center">To attract more business to my low voltage company</ul>
 		<ul align="center">To show that I now will have the capability to build websites along with the work that I already do.</ul>
 		<ul align="center">To ultimately end up on a website that I will build from scratch for my business Integrated Media Solutions so my targeted audience will know more about me.</ul>
 		<ul align="center">To state my endgame of my career out look which will be working with DoD in a cybrer security division somewhere.</ul>
+	</div>
+		<br>
+		<h2 align="center"><strong></strong><font size="5">Persona:</font></h2>
+			<p align="center">I am Efren Gonzales I am a 39 year old low voltage technician and business owner. I have been in the low voltage field for almost 23 years.
+			I sell, service and install everthing from simple phone systems to full on business network solutions. Small home security systems and CCTV systems to
+			full on multi zone and channel commercial security systems. Fire alarm systems for businesses. Home entertainment and home automation systems. I integrate
+			all that technology together and can put the controls of those systems into a simple easy to use app on your phone or computer. I am building this PWP to
+			showcase my knowledge, skills, certifications and achievements in hopes of getting more work via contracts with other small business owners or individuals.
+			I would like for potential clients to reference my PWP to see what I am capiable of to make them feel more comfortable in hiring me to do their work. I would
+			like to continue to update this PWP with the new work in computer coding and other work that I do with my business in hopes of eventually landing a job
+			somewhere within the Department of Defence doing cyber security work for them within the next 5-7 years. During that time it is my goal to have my current
+			business be able to keep me busy enough with this new service I will be able to provide of building websites which in turn will get me the experience needed
+			to continue to move forward in that field leaving my small business to run itself while I pursue my ultimate goal working with DoD writing code for them.</p>
 	</body>
 </html>
